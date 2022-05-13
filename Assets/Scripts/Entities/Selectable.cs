bcb5a15d19d34a821ed81b3449dbed1372335286
war10cken <44408023+war10cken@users.noreply.github.com>
2022-05-13 15:57:48 +0300

    Move to Entities folder

---
+++ Assets/Scripts/Entities/Selectable.cs
@@ -0,0 +1,47 @@
+#region
+
+using Enemies;
+using UnityEngine;
+
+#endregion
+
+namespace Entities
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Selectable : MonoBehaviour
+    {
+        private Rigidbody _rigidbody;
+        private float _damage;
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.rigidbody is not null)
+            {
+                if (other.gameObject.TryGetComponent(out Enemy enemy))
+                {
+                    enemy.TakeDamage(_damage);
+                }
+
+                if (other.gameObject.TryGetComponent(out Ground ground))
+                {
+                    _rigidbody.AddForce(transform.up * 1000f, ForceMode.Force);
+                }
+                
+                if (_rigidbody.mass > other.rigidbody.mass)
+                {
+                    Destroy(other.gameObject);
+                }
+            }
+        }
+
+        public static void SetDamage(float value, Selectable item)
+        {
+            item._damage = value;
+        }
+    }
+}
