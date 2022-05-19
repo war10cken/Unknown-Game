0907cc1197836ebfca58b73c5b8c6d6f59eb33f0
war10cken <44408023+war10cken@users.noreply.github.com>
2022-05-19 17:01:04 +0300

    Update LoadScene.cs

--- Assets/Scripts/Utilities/LoadScene.cs
+++ Assets/Scripts/Utilities/LoadScene.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,21 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
+    [SerializeField] private Player.Player _player;
+    
+    private void OnEnable()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            if (_player.TryGetComponent(out AudioSource audioSource))
+            {
+                audioSource.enabled = false;
+            }
+        
+            Time.timeScale = 0;
+        }
+    }
+
     public void Load(int index)
     {
         SceneManager.LoadScene(index);
