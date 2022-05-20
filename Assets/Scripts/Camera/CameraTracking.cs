7ec7d938b533407af585e57c711b06ac29a419a8
KirillPolytech <kirill2640@yandex.ru>
2022-05-20 14:09:40 +0300

    0.73

--- Assets/Scripts/Camera/CameraTracking.cs
+++ Assets/Scripts/Camera/CameraTracking.cs
@@ -13,7 +13,11 @@ public class CameraTracking : MonoBehaviour
     private Vector3 Velocity = new(0, 0, 0);
     void Update()
     {
-        Vector3 newPosition = new(Player.transform.position.x, Player.transform.position.y + OffsetPositionY, Player.transform.position.z - OffsetPositionZ);
+        Vector3 OFFSET = new(0, OffsetPositionY, OffsetPositionZ);
+        Vector3 newPosition = (Player.transform.position - transform.forward) + OFFSET;
+        //Vector3 newPosition = new(Player.transform.position.x, Player.transform.position.y + OffsetPositionY, Player.transform.position.z - OffsetPositionZ);
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref Velocity, CameraTrackingSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.LookRotation(Player.transform.forward);
         Debug.DrawRay(transform.position, -transform.forward, Color.red);
     }
 }
